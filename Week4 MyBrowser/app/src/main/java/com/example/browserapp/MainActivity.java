7b0a9b240e482c6170d7f0c310b7077b6c953a30
U180709044 <ozgunnesim@gmail.com>
2021-10-24 02:09:22 +0300

    week 4

---
+++ Week4 MyBrowser/app/src/main/java/com/example/browserapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.browserapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        EditText txtAddress = findViewById(R.id.txtAddress);
+        Button button = findViewById(R.id.btnGo);
+
+        if (getIntent() != null && getIntent().getData() != null){
+            txtAddress.setText(getIntent().getData().toString());
+            webView.loadUrl(getIntent().getData().toString());
+        }
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl("http://" + txtAddress.getText());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
